809d1b37f170412990d51676624ddb32e73259ec
iehtian <1850453937@qq.com>
2023-05-20 17:14:20 +0800

    差分处理数组区间，还需巩固

--- CCF202203-2.cpp
+++ CCF202203-2.cpp
@@ -4,31 +4,31 @@ using namespace std;
 int main(){
     int n, m, k;
     cin >> n >> m >> k;
-    int a[n],b[n];
-    for (int i = 0; i < n; i++)
+    int a[n + 1], b[n + 1];
+    a[0] = 0, b[0] = 0;
+    for (int i = 1; i < n+1; i++)
     {
         cin >> a[i] >> b[i];
+        a[i] -= k;
         b[i] = a[i] - b[i];
     }
-    int t=0;
-    int c[n];
+    int d[1000000] = {0};
+    for (int i = 1; i < n+1; i++)
+    {
+        if (a[i]>=0)
+        {
+            d[max(0,b[i]+1)]++;
+            d[a[i] + 1]--;
+        }
+    }
+    for (int i = 1; i < 1000000; i++)
+    {
+        d[i] += d[i - 1];
+    }
     for (int i = 0; i < m; i++)
     {
-        int x;
-        cin >> x;
-        int time = x + k;
-        int *p = lower_bound(a+t, a + n, time);
-        t = p - a;
-        memcpy(c, b, sizeof(b));
-        int con = 0;
-        for (int j = p-a; j < n; j++)
-        {
-            if (time>c[j])
-            {
-                con++;
-            }
-        }
-        cout << con << endl;
+        int tem;
+        cin >> tem;
+        cout << d[tem] << endl;
     }
-    
 }
