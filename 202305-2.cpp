3e23b19a33e8f074beaf6ba63a5d1fb07ccb511a
iehtian <1850453937@qq.com>
2023-07-16 23:32:01 +0800

    90分代码，运算有随机性，可能是85分

--- 202305-2.cpp
+++ 202305-2.cpp
@@ -1,30 +1,15 @@
-#include<algorithm>
-#include<iostream>
-#include<map>
-#include<unordered_map>
-#include<stack>
-#include<queue>
-#include<deque>
-#include<string>
-#include<string.h>
+#include <bits/stdc++.h>
 using namespace std;
-const int maxnum=1e9;
-struct shuju
-{
-    int shang, yu;
-};
-
 int main(){
     int n,d;
     cin>>n>>d;
-    int q[n][d],k[n][d],v[n][d];
+    long long q[n][d],k[n][d],v[n][d];
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < d; j++)
         {
             cin>>q[i][j];
         }
-        
     }
     for (int i = 0; i < n; i++)
     {
@@ -32,7 +17,6 @@ int main(){
         {
             cin>>k[i][j];
         }
-        
     }
     for (int i = 0; i < n; i++)
     {
@@ -41,91 +25,35 @@ int main(){
             cin>>v[i][j];
         }
     }
-    int w[n];
+    long long w[n];
     for (int i = 0; i < n; i++)
     {
         cin>>w[i];
     }
-    int ii=0,jj=0;
-    int zhuanzhi[d][n];
-    for (int i = 0; i < d; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            zhuanzhi[ii][jj]=k[j][i];
-            if (jj%n==n-1)
-            {
-                ii++;
-                jj=0;
-            }
-            else{
-                jj++;
-            }
-        }
-    }
-    // long long qk[n][n],res[n][d];
-    shuju qkk[n][n];
-    int iii=0;
     for (int i = 0; i < n; i++)
     {
+            long long qk[n]={0};
         for (int j = 0; j < n; j++)
         {
-            // qk[i][j]=0;
-            long long tem = 0;
-            for (int jjj = 0; jjj < d; jjj++)
+            for (int m = 0; m < d; m++)
             {
-                
-                tem+=q[i][jjj]*zhuanzhi[jjj][iii];
-            }
-            qkk[i][j].shang = tem / maxnum;
-            qkk[i][j].yu = tem % maxnum;
-            iii++;
-            if (iii%n==0)
-            {
-                iii=0;
+                qk[j]+=q[i][m]*k[j][m];
             }
         }
-    }
-    ii=0;
-    for (int i = 0; i < n; i++)
-    {
         for (int j = 0; j < n; j++)
         {
-            long long tem = qkk[i][j].shang * maxnum + qkk[i][j].yu;
-            // qk[i][j]*=w[ii];
-            tem *= w[ii];
-            qkk[i][j].shang = tem / maxnum;
-            qkk[i][j].yu = tem % maxnum;
+            qk[j]*=w[i];
         }
-        ii++;
-    }
-    iii=0;
-    for (int i = 0; i < n; i++)
-    {
         for (int j = 0; j < d; j++)
         {
-            long long res = 0;
-            for (int jjj = 0; jjj < n; jjj++)
+            long long res=0;
+            for (int m = 0; m < n; m++)
             {
-                long long tem = qkk[i][jjj].shang * maxnum + qkk[i][jjj].yu;
-                res += tem * v[jjj][iii];
-                
-                }
-                cout << res << " ";
-                iii++;
-                if (iii%d==0)
-                {
-                    iii=0;
-                }
+                res+=qk[m]*v[m][j];
+            }
+            cout <<res<<" ";
         }
-        cout<<endl;
+        cout << endl;
     }
-    // for (int i = 0; i < n; i++)
-    // {
-    //     for (int j = 0; j < d; j++)
-    //     {
-    //         cout<<res[i][j]<<" ";
-    //     }
-    //     cout<<endl;
-    // }
+    
 }
--- test.cpp
+++ test.cpp
@@ -11,14 +11,13 @@ using namespace std;
 int main(){
     int n,d;
     cin>>n>>d;
-    int q[n][d],k[n][d],v[n][d];
+    long long q[n][d],k[n][d],v[n][d];
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < d; j++)
         {
             cin>>q[i][j];
         }
-        
     }
     for (int i = 0; i < n; i++)
     {
@@ -26,7 +25,6 @@ int main(){
         {
             cin>>k[i][j];
         }
-        
     }
     for (int i = 0; i < n; i++)
     {
@@ -40,73 +38,30 @@ int main(){
     {
         cin>>w[i];
     }
-    int ii=0,jj=0;
-    int zhuanzhi[d][n];
-    for (int i = 0; i < d; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            zhuanzhi[ii][jj]=k[j][i];
-            if (jj%n==n-1)
-            {
-                ii++;
-                jj=0;
-            }
-            else{
-                jj++;
-            }
-        }
-    }
-    long long qk[n][n],res[n][d];
-    int iii=0;
+    int qk[n]={0};
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            qk[i][j]=0;
-            for (int jjj = 0; jjj < d; jjj++)
+            for (int m = 0; m < d; m++)
             {
-                qk[i][j]+=q[i][jjj]*zhuanzhi[jjj][iii];
-            }
-            iii++;
-            if (iii%n==0)
-            {
-                iii=0;
+                qk[j]+=q[i][m]*k[j][m];
             }
         }
-    }
-    ii=0;
-    for (int i = 0; i < n; i++)
-    {
         for (int j = 0; j < n; j++)
         {
-            qk[i][j]*=w[ii];
+            qk[j]*=w[i];
         }
-        ii++;
-    }
-    iii=0;
-    for (int i = 0; i < n; i++)
-    {
         for (int j = 0; j < d; j++)
         {
-            res[i][j]=0;
-                for (int jjj = 0; jjj < n; jjj++)
-                {
-                    res[i][j]+=qk[i][jjj]*v[jjj][iii];
-                }
-                 iii++;
-                if (iii%d==0)
-                {
-                    iii=0;
-                }
+            long long res=0;
+            for (int m = 0; m < n; m++)
+            {
+                res+=qk[m]*v[m][j];
+            }
+            cout <<res<<" ";
         }
+        cout << endl;
     }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < d; j++)
-        {
-            cout<<res[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+    
 }
